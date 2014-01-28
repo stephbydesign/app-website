a8944ff4d91f96902b0f9b87849910501d92755d
Stephanie Albert <salbert61@gmail.com>
2014-01-28 02:02:53 -0500

    Pop-up video works!
    She’s not ugly, she just looks that way.

--- _includes/js/video.js
+++ _includes/js/video.js
@@ -1,15 +1,16 @@
-var $dialog = $('.video');
-var $placeholder = $('.video-placeholder');
+var $dialog = $('#video');
+var $placeholder = $('#video-placeholder');
 
-dialogPolyfill.registerDialog($dialog.get(0));
+var dialog = document.querySelector('dialog');
+  dialogPolyfill.registerDialog(dialog);
 
-$('.btn-open').on ('click', function () {
+$('#btn-open').on ('click', function () {
   $placeholder.html('<iframe src="http://player.vimeo.com/video/78532154?color=e90101&amp;autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
 
   $dialog.get(0).showModal();
 });
 
-$('.btn-close').on('click', function () {
+$('#btn-close').on('click', function () {
   $dialog.get(0).close();
   $placeholder.html('');
 });
