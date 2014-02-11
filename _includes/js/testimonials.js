e99eaedda5d89b9a07bf2feed1805547f4bed4bc
Stephanie Albert <salbert61@gmail.com>
2014-02-10 23:13:14 -0500

    Renamed variable to match classname
    Testimonials

--- _includes/js/testimonials.js
+++ _includes/js/testimonials.js
@@ -1,21 +1,12 @@
-//var rando = Math.round( Math.random() * 2);
-//var file = 'praise-' + rando + '.html';
-
-//$('.rando-praise').load(file);
-
-// Automaticall load the dinos.json file as soon as the page loads
-var testimonialData = $.ajax('testimonials.json', {
-  //Tell JQuery we are loading a JSON file and to parse it properly
+// Automatically load the testimonials.json file as soon as the page loads
+var testimonialsData = $.ajax('testimonials.json', {
   dataType: 'json'
 });
 
-// Because AJAX takes time to download things off the internet
-//    .done() will be triggered only after JQuery has finished downloading
-// var rando = dino = Math.round( Math.random() * 2); to select the same variables and apply the same function
 testimonialData.done(function (data) {
   var testimonials = Math.round( Math.random() * data.length - 1 );
 
-  $('.testimonial-praise').html(data[testimonials].praise);
-  $('.testimonial-name').html(data[testimonials].name);
-  $('.testimonial-position').html(data[testimonials].position);
+  $('.testimonials-praise').html(data[testimonials].praise);
+  $('.testimonials-name').html(data[testimonials].name);
+  $('.testimonials-position').html(data[testimonials].position);
 });
